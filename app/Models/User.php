4cc611b191409bb31727df33dcaa4d1d7be8ec21
Marcus <marcuspaulo84>
2016-09-22 22:11:32 +0000

    GivenName

--- app/Models/User.php
+++ app/Models/User.php
@@ -26,4 +26,12 @@ class User extends Authenticatable
     protected $hidden = [
         'password', 'remember_token',
     ];
+
+    /*public function isSuperAdmin(){
+        return
+    }*/
+
+    public function roles(){
+
+    }
 }
